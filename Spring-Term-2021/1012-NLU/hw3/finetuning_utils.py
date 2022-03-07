0fa05ef828fb6b5d68d29eff9784178b3ae70cd7
Giulio Duregon <giulio@10-19-166-18.dynapool.wireless.nyu.edu>
2022-03-07 14:40:39 -0500

    Added Hw3 For NLU

---
+++ Spring-Term-2021/1012-NLU/hw3/finetuning_utils.py
@@ -0,0 +1,18 @@
+
+
+def compute_metrics(eval_pred):
+    """Computes accuracy, f1, precision, and recall from a 
+    transformers.trainer_utils.EvalPrediction object.
+    """
+    labels = eval_pred.label_ids
+    preds = eval_pred.predictions.argmax(-1)
+
+    ## TODO: Return a dictionary containing the accuracy, f1, precision, and recall scores.
+    ## You may use sklearn's precision_recall_fscore_support and accuracy_score methods.
+    pass
+
+def model_init():
+    """Returns an initialized model for use in a Hugging Face Trainer."""
+    ## TODO: Return a pretrained RoBERTa model for sequence classification.
+    ## See https://huggingface.co/transformers/model_doc/roberta.html#robertaforsequenceclassification.
+    pass
